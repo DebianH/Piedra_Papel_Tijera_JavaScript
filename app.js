aae6d26d168657f000531eb8cef543df2b45a293
DebianH <davidhernandez_s@hotmail.com>
2022-01-11 06:48:28 -0500

    commit final

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+
 function getComputerChoice() {
     const choices = ["r", "p", "s"];
     const randomNumber = Math.floor(Math.random() * 3);
@@ -21,6 +22,8 @@ function convertToWord(letter) {
 }
 
 function win(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice); // Hacemos el codigo mas legible agregando una variable
+
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
@@ -30,10 +33,15 @@ function win(userChoice, computerChoice) {
 
     result_p.innerHTML = `${convertToWord(userChoice)} ${letraUser} vence a ${convertToWord(computerChoice)} ${letraIa} You Win! 🔥`;
 
-    document.getElementById(userChoice).classList.add('green-glow'); //Agremados un border si ganamos
-    setTimeout(function(){ document.getElementById(userChoice).classList.remove('green-glow'); }, 2000) //Agregamos un tiempo un borde
+    //document.getElementById(userChoice).classList.add('green-glow'); //Agremados un border si ganamos
+    //setTimeout(function(){ document.getElementById(userChoice).classList.remove('green-glow'); }, 2000) //Agregamos un tiempo un borde
+
+    userChoice_div.classList.add('green-glow');
+    setTimeout(function(){ userChoice_div.classList.remove('green-glow'); }, 2000);
 }
 function lose(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice);
+
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
@@ -42,13 +50,18 @@ function lose(userChoice, computerChoice) {
     const letraIa = "IA".fontsize(3).sub();
 
     result_p.innerHTML = `${convertToWord(userChoice)} ${letraUser} pierde ${convertToWord(computerChoice)} ${letraIa} You Lose! ❌`;
+    userChoice_div.classList.add('red-glow');
+    setTimeout(function(){ userChoice_div.classList.remove('red-glow'); }, 2000);
 }
 function draw(userChoice, computerChoice) {
+    const userChoice_div = document.getElementById(userChoice);
 
     const letraUser = "User".fontsize(3).sub();
     const letraIa = "IA".fontsize(3).sub();
 
     result_p.innerHTML = `${convertToWord(userChoice)} ${letraUser} iguales ${convertToWord(computerChoice)} ${letraIa} EMPATE! 🏆`;
+    userChoice_div.classList.add('orange-glow');
+    setTimeout(function(){ userChoice_div.classList.remove('orange-glow'); }, 2000);
 }
 
 function game(userChoice){
